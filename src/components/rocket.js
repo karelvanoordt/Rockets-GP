95bd500e745b329b15bfc4d289527a4780486e31
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-12-15 15:28:38 -0500

    Add Reserve and Cancel Rocket functions

--- src/components/rocket.js
+++ src/components/rocket.js
@@ -1,17 +1,68 @@
-export default function Rocket(rocket) {
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { bookRocket, cancelRocket, updateStatus } from '../redux/pages/rockets';
+
+export default function Rocket({ rocket }) {
+
+    const dispatch = useDispatch();
+
+    function eventHandler(e) {
+        const btn = e.target.className;
+        const { id } = e.target;
+
+        if (btn === 'state') {
+            dispatch(bookRocket(rocket.id));
+            dispatch(updateStatus(rocket.id));
+            let arr = [];
+            const obj = {
+                status: rocket.status,
+                id: rocket.id,
+                reserved: false,
+                bg: 'status_2',
+            };
+            arr.push(obj);
+      if (localStorage.getItem('statusData') !== null) {
+        const localArr = JSON.parse(localStorage.getItem('statusData'));
+        arr.forEach((item) => {
+          localArr.forEach((items) => {
+            if (items.id !== item.id) {
+              arr.push(items);
+            } else {
+              arr = arr.filter((ite) => ite.id === item.id);
+            }
+          });
+        });
+      }
+      localStorage.setItem('statusData', JSON.stringify(arr));
+    } else if (btn === 'state_2') {
+      dispatch(cancelRocket(rocket.id));
+      dispatch(updateStatus(rocket.id));
+      const arr1 = JSON.parse(localStorage.getItem('statusData'));
+      const index = arr1.findIndex((item) => item.id === id);
+      arr1.splice(index, 1);
+      localStorage.setItem('statusData', JSON.stringify(arr1));
+    }
+  };
+
+  function addBTN(state) {
+    if (state) {
+      return <button type="button" id={rocket.id} className="state" onClick={eventHandler}>Reserve Rocket</button>;
+    }
+    return <button type="button" id={rocket.id} className="state_2" onClick={eventHandler}>Cancel Reservation</button>;
+  };
 
     return (
         <div className="rocket-container">
             <div className="rocket-img-col">
                 <div className="rocket-img-cont">
-                    <img className="rocket-img" src={rocket.rocket.flickr_images[0]} alt="SpaceX rocket"/>
+                    <img className="rocket-img" src={rocket.flickr_images[0]} alt="SpaceX rocket"/>
                 </div>
             </div>    
 
             <div className="rocket-info-col">
-                <h2 className="rocket-title">{rocket.rocket.rocket_name}</h2>
-                <p className="rocket-description"><span className="rocket-reserved"></span>{rocket.rocket.description}</p>
-                <button className="rocket-reserve-btn" type="submit">Reserve Rocket</button>
+                <h2 className="rocket-title">{rocket.rocket_name}</h2>
+                <p className="rocket-description"><span className="rocket-reserved"></span>{rocket.description}</p>
+                <span className="rocket-reserve-btn">{addBTN(rocket.reserved)}</span>
             </div>
         </div>
     )
--- src/redux/pages/rockets.js
+++ src/redux/pages/rockets.js
@@ -1,5 +1,9 @@
 const rocketsURL = 'https://api.spacexdata.com/v3/rockets';
 const GET_ROCKETS = 'spaceHub/api/GET_ROCKETS';
+const BOOK_ROCKET = 'spaceHub/rockets/BOOK_ROCKET';
+const CANCEL_ROCKET = 'spaceHub/rockets/CANCEL_ROCKET';
+const UPDATE_STATUS = 'spaceHub/rockets/UPDATE_STATUS';
+
 
 const initialState = [];
 
@@ -8,17 +12,71 @@ export const getRockets = (payload) => ({
     payload
 });
 
+export const bookRocket = (payload) => ({
+    type: BOOK_ROCKET,
+    payload
+});
+
+export const cancelRocket = (payload) => ({
+    type: CANCEL_ROCKET,
+    payload
+});
+
+export const updateStatus = (payload) => (
+    {
+      type: UPDATE_STATUS,
+      payload,
+    }
+  );
+
+let localDATA = [{
+    id: '',
+    reserved: true,
+    status: 'Cancel Reservation',
+    Bg: 'status',
+}];
+
 export const getROCKETS = () => async (dispatch) => {
+
+    if(localStorage.getItem('statusData')) {
+        localDATA = JSON.parse(localStorage.getItem('statusData'));
+    }
     const rockets = await fetch(rocketsURL)
         .then((res) => res.json());
     const ID = Object.keys(rockets);
     const rocketsArr = [];
+
+    function reserveValue(id) {
+        const index = localDATA.findIndex((item) => item.id === id);
+        if (index !== -1) {
+            return localDATA[index].reserved;
+        }
+        return true;
+    };
+
+    function statusValue(reserve) {
+        if (reserve) {
+          return 'Reserve Rocket';
+        }
+        return 'Cancel Reservation';
+    }
+    
+      function BgValue(reserve) {
+        if (reserve) {
+          return 'status';
+        }
+        return 'status_2';
+      }
+
     ID.map((key) => rocketsArr.push({
         key: key,
         rocket_name: rockets[key].rocket_name,
         description: rockets[key].description,
         flickr_images: rockets[key].flickr_images,
-        reserved: false,
+        reserved: reserveValue(rockets[key].rocket_id),
+        status: statusValue(reserveValue(rockets[key].rocket_id)),
+        Bg: BgValue(reserveValue(rockets[key].rocket_id)),
+
     }));
     dispatch(getRockets(rocketsArr));
 };
@@ -26,12 +84,30 @@ export const getROCKETS = () => async (dispatch) => {
 
 const rocketsReducer = (state = initialState, action) => {
     switch (action.type) {
-      case GET_ROCKETS:
-            return action.payload;
-      default:
-        return state;
-    }
-};
-
+        case GET_ROCKETS:
+          return action.payload;
+        case BOOK_ROCKET:
+        case CANCEL_ROCKET:
+          return state.map((rocket) => {
+            if (rocket.id !== action.payload) {
+              return rocket;
+            }
+            return { ...rocket, reserved: !rocket.reserved };
+          });
+        case UPDATE_STATUS:
+          return state.map((rocket) => {
+            if (rocket.id === action.payload) {
+              if (rocket.status === 'Reserve Rocket') {
+                return { ...rocket, status: 'Cancel Reservation', Bg: 'status_2' };
+              } if (rocket.status === 'Cancel Reservation') {
+                return { ...rocket, status: 'Reserve Rocket', Bg: 'status' };
+              }
+            }
+            return rocket;
+          });
+        default:
+          return state;
+      }
+    };
 export default rocketsReducer;
 
