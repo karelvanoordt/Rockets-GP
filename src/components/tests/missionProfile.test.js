6ea4cf63358540b783917a6f0f5d8a02b0eac270
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-12-16 10:54:52 -0500

    Add component tests

---
+++ src/components/tests/missionProfile.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MissionProfile from '../missionProfile';
+
+const missionProfile = (
+        <MissionProfile />
+)
+
+it('renders correctly', () => {
+   expect(missionProfile).toMatchSnapshot();
+});
---
+++ src/components/tests/rocketProfile.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RocketProfile from '../rocketProfile';
+
+const rocketProfile = (
+        <RocketProfile />
+)
+
+it('renders correctly', () => {
+   expect(rocketProfile).toMatchSnapshot();
+});
